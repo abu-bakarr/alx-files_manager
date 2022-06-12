507dc3b9d9f5294c89cd7852b3621a98b7399b45
abu-bakarr <abangura14@yahoo.com>
2022-06-12 19:23:36 +0000

    Add: routes/index.js, controllers/UsersController.js

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
 
 function controllerRoute(app) {
   const router = express.Router();
@@ -15,6 +16,18 @@ function controllerRoute(app) {
   // should return the number of users and files in DB
   router.get('/stats', (req, res) => {
     AppController.getStats(req, res);
+  });
+
+  // User Controller
+
+  // should create a new user in DB
+  router.post('/users', (req, res) => {
+    UsersController.postNew(req, res);
+  });
+
+  // should retrieve the user base on the token used
+  router.get('/users/me', (req, res) => {
+    UsersController.getMe(req, res);
   });
 }
 
